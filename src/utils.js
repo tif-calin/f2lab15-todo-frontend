37363122e188080dafba8b5ef0c0ecb2aff5b97f
tif-calin <culitif@tuta.io>
2021-05-17 16:38:33 -0700

    implement api utils functions for api endpoints

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,5 @@
 import request from 'superagent';
 
-export async function getMyTodos() {
-  const response = await request.get('/api/me/todos');
-  return response.body;
-}
-
 export async function signUp(user) {
   const response = await request
     .post('/api/auth/signup')
@@ -14,6 +9,7 @@ export async function signUp(user) {
   if (response.status === 400) {
     throw response.body;
   }
+  
   return response.body;
 }
 
@@ -26,6 +22,57 @@ export async function signIn(user) {
   if (response.status === 400) {
     throw response.body;
   }
+  
   return response.body;
 }
 
+export async function getMyTodos() {
+  const response = await request
+    .get('/api/me/todos')
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
+export async function addTodo(todo) {
+  const response = await request 
+    .post('/api/todos')
+    .set('Authorization', window.localStorage.getItem('TOKEN'))
+    .send(todo);
+
+  return response.body;
+}
+
+export async function getSharedTodos() {
+  const response = await request 
+    .get('/api/todos')
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
+export async function completeTodo(todo) {
+  const response = await request 
+    .put(`/api/todos/${todo.id}/completed`)
+    .set('Authorization', window.localStorage.getItem('TOKEN'))
+    .send(todo);
+
+  return response.body;
+}
+
+export async function shareTodo(todo) {
+  const response = await request 
+    .put(`/api/todos/${todo.id}/shared`)
+    .set('Authorization', window.localStorage.getItem('TOKEN'))
+    .send(todo);
+
+  return response.body;
+}
+
+export async function deleteTodo(id) {
+  const response = await request 
+    .delete(`/api/todos/${id}`)
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+  
+  return response.body;
+}
